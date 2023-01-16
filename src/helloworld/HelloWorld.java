10fc0f58a2f3470c63b409c5af34f20af4216d4f
aariawann <aariawan@uoguelph.ca>
2023-01-16 12:06:39 -0500

    helloWorld commit
    helloWorld

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/** *************************************
+ * Project:
+ * Programmer: Aris Ariawan
+ * Date: Month #, 2023
+ * Program: ----.java
+ ************************************** */
+package helloworld;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+        
+
+        
+    }
+    
+}
